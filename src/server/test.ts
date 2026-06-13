35ba6701c3cf52bd676f6fddd71fd2b78d48209d
Dipali Sharma <sharmadipali2000@gmail.com>
2026-06-13 05:45:49 +0530

    Add tsx dependency and update test script to fetch Gmail threads

--- src/server/test.ts
+++ src/server/test.ts
@@ -1,1 +1,8 @@
-console.log("Hello");
+import { corsair } from "./corsair";
+import "dotenv/config";
+const main = async () => {
+  const res = await corsair.withTenant("dev").gmail.db.threads.list({});
+  console.log(res);
+};
+
+main();
