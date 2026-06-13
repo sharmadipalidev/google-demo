9a6b947bca04556d3e565ad03af2b76c08053317
Dipali Sharma <sharmadipali2000@gmail.com>
2026-06-13 10:44:17 +0530

    Implement webhook handling for POST and GET requests in route.ts

--- src/server/test.ts
+++ src/server/test.ts
@@ -1,7 +1,9 @@
 import { corsair } from "./corsair";
 import "dotenv/config";
 const main = async () => {
-  const res = await corsair.withTenant("dev").gmail.db.threads.search({});
+  const res = await corsair
+    .withTenant("dev")
+    .googlecalendar.api.events.create({});
   console.log(res);
 };
 
