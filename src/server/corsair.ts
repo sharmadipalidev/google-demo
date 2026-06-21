b36e2a79e740cb15baeff8c2ba378eea1a1015c7
Dipali Sharma <sharmadipali2000@gmail.com>
2026-06-21 15:50:52 +0530

    fix: remove console logs that crash Vercel build

--- src/server/corsair.ts
+++ src/server/corsair.ts
@@ -5,8 +5,7 @@ import { googlecalendar } from "@corsair-dev/googlecalendar";
 import { conn } from "./db/index";
 import { env } from "@/env";
 
-console.log("CORS DB CONNECTION KEYS:", Object.keys(conn || {}));
-console.log("CORSAIR INITIALIZED AT:", Date.now(), "WITH EXACT KEK:", env.CORSAIR_KEK.substring(0, 5) + "...");
+
 
 export const corsair = createCorsair({
   plugins: [gmail(), googlecalendar()],
